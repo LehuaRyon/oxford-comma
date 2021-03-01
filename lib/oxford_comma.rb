0da523c8183a8a82ddd53f7dfdb5004bb4d36c76
Jasmine Ryon <jryon12@priorypride.net>
2021-03-01 13:27:50 -0800

    commit tree b32efcf8d76a4d6f8d307ecc832b411179eee6bf

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.count == 2
+        array.join(" and ")
+    elsif array.count < 2
+        array.join
+    elsif array.count > 2
+        element = array.pop
+        array.join(", ") << (", and ") + element      
+    end    
 end
+
